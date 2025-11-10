f60d2afc880f2e7db801c233cb6deca2030d7041
Nico <nicob0unico@gmail.com>
2025-11-10 22:11:16 +0330

    Initial commit

--- register.php
+++ register.php
@@ -1,15 +1,15 @@
-  <?php
-  error_reporting(E_ALL);
+<?php
+error_reporting(E_ALL);
 ini_set('display_errors', 1);
-  $servername = "localhost";
-  $dbusername = "root";
-$dbpassword = "";   
+$servername = "localhost";
+$dbusername = "root";
+$dbpassword = "";
 $dbname = "user_database";
 
 $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
 $username = $_POST['username'];
@@ -23,14 +23,13 @@ $stmt = $conn->prepare($sql);
 $stmt->bind_param("sss", $username, $email, $hashedPassword);
 
 if ($stmt->execute()) {
-    // 5. Return user info (without password)
-    echo json_encode([
-        "id" => $stmt->insert_id,
-        "username" => $username,
-        "email" => $email
-    ]);
+  echo json_encode([
+    "id" => $stmt->insert_id,
+    "username" => $username,
+    "email" => $email
+  ]);
 } else {
-    echo "Error: " . $stmt->error;
+  echo "Error: " . $stmt->error;
 }
 
 $stmt->close();
